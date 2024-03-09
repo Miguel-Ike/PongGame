31ac9643f97de223de5706354560a30537d5e356
Miguel Angel Martinez Rodriguez <miguel_angel-m@outlook.com>
2024-03-09 10:51:42 -0400

    Initial commit

---
+++ src/main/java/toji/PongGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package toji;
+
+/**
+ *
+ * @author Miguel_Angel
+ */
+public class PongGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
